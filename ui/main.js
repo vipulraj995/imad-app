c223f5b17be887273997173ddd44137f4ca2a40a
aishwaryasurwase <31077384+aishwaryasurwase@users.noreply.github.com>
2017-08-19 21:42:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,8 +10,8 @@ var img=document.getElementById('madi');
 marginLeft=0;
 function moveRight() {
     marginLeft=marginLeft + 10;
-    img.style.marginLeft = marginLeft+'px;'
+    img.style.marginLeft = marginLeft+'px';
 }
 img.onclick=function() {
-    var interval=setInterval(moveRight,100)
+    var interval=setInterval(moveRight,100);
 };
