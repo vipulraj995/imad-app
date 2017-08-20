fe9b8a620aa87afd4ca1fb33b29222a6f885533a
aishwaryasurwase <31077384+aishwaryasurwase@users.noreply.github.com>
2017-08-20 18:00:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,3 @@
-console.log('Loaded!');
-
-//change the text of the main-text div
-
-var element= document.getElementById('main-text');
-element.innerHTML='Vipul';
-
-//move the image
-var img=document.getElementById('madi');
-marginLeft=0;
-function moveRight() {
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft = marginLeft+'px';
-}
-img.onclick=function() {
-    var interval=setInterval(moveRight,50);
-};
 
 //counter code
 
