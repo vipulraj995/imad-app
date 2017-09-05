0e82e887bcfb0dcf35db256af8391c2569b13030
aishwaryasurwase <31077384+aishwaryasurwase@users.noreply.github.com>
2017-09-05 15:56:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ submit.onclick = function()
 {
     var request = new XMLHttpRequest();
     
-    request.onreadystatechange =function()
+    request.onreadystatechange = function()
     {
         if(request.readyState === XMLHttpRequest.DONE) {
         if(request.status ===200)
@@ -49,5 +49,5 @@ submit.onclick = function()
 };
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
- request.open('GET','http://vipulraj995.imad.hasura-app.io/submit-name?name='+name,true);
+ request.open('GET','http://vipulraj995.imad.hasura-app.io/submit-name?name='+name. true);
 };
