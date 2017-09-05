73251316ac20381a8ae0d8c2efae575e99956810
aishwaryasurwase <31077384+aishwaryasurwase@users.noreply.github.com>
2017-09-05 13:59:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.get('/', function (req, res) {
 
 function hash(input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('bin');
+    return hashed.toString('binary');
 }
 
 app.get('/hash/:input', function (req,res) {
